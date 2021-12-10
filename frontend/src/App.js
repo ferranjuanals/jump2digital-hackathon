24322a78a9fe20b6c263f217550911e167fac501
arstix <runjie30@gmail.com>
2021-12-10 17:07:11 +0100

    statistics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import SelectUpdate from './components/SelectUpdate/SelectUpdate.jsx';
 import MonthValues from './components/MonthValues/MonthValues';
+import MonthStadistics from './components/MonthStadistics/MonthStadistics';
 
 function App() {
   return (
     <div className="App">
+      <div className="App__top">
         <Header />  
         <SelectUpdate />
         <MonthValues />
+      </div>
+      <div className="App__bottom">
+        <MonthStadistics />
+      </div>
     </div>
   );
 }
