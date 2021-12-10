248b05c27f8ad0e7eb957d4c81480e1402d47a86
arstix <runjie30@gmail.com>
2021-12-10 13:53:49 +0100

    fix-header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Header from './components/Header/Header.jsx';
 import SelectUpdate from './components/SelectUpdate/SelectUpdate.jsx';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
