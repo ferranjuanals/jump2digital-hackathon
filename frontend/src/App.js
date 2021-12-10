f0f7a8f9cb09f97e1c6af59b7ec3169f49deb2f8
Lola Rufino <lola.ruf.ar@gmail.com>
2021-12-10 16:56:35 +0100

    Add logic for context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import SelectUpdate from './components/SelectUpdate/SelectUpdate.jsx';
 import MonthValues from './components/MonthValues/MonthValues';
+import {monthContext} from '../src/context/monthContext';
+import { useState } from 'react';
+import monthsArray from '../src/assets/mockedData.json';
 
 function App() {
+  const [month, setMonth] = useState('january2020');
+  const [months, setMonths] = useState(monthsArray);
+
   return (
+    <monthContext.Provider value={{month, setMonth, months, setMonths}}>
     <div className="App">
         <Header />  
         <SelectUpdate />
         <MonthValues />
     </div>
+    </monthContext.Provider>
   );
 }
 
--- frontend/src/components/MonthValues/MonthValues.jsx
+++ frontend/src/components/MonthValues/MonthValues.jsx
@@ -1,20 +1,27 @@
-import React from "react";
-import "../MonthValues/monthvalues.css"
+import React, { useContext} from "react";
+import "../MonthValues/monthvalues.css";
+import {monthContext} from '../../context/monthContext';
 
 function MonthValues() {
+  const {month, setMonth} = useContext(monthContext);
+  const {months, setMonths} = useContext(monthContext);
+
+ const currentMonth = months.find((element) => element.name === month);
+ console.log(currentMonth);
+
   return ( 
     <>
     <div className="monthvalues__card">
       <h3 className="monthvalues__title">Accepted requests</h3>
-      <p className="monthvalues__data">There are<span className="monthvalues__value">5</span> total accepted resquest </p>
+      <p className="monthvalues__data">There are <span className="monthvalues__value">{currentMonth.acceptedRequests}</span> total accepted requests </p>
     </div>  
     <div className="monthvalues__card monthvalues__card__green">
       <h3 className="monthvalues__title">Return time</h3>
-      <p className="monthvalues__data">The average return time is <span className="monthvalues__value">10</span> days</p>
+      <p className="monthvalues__data">The average return time is <span className="monthvalues__value">{currentMonth.loanReturnTime}</span> days</p>
     </div>  
     <div className="monthvalues__card">
       <h3 className="monthvalues__title">Budget</h3>
-      <p className="monthvalues__data">The total budget is <span className="monthvalues__value">1.500$</span></p>
+      <p className="monthvalues__data">The total budget is <span className="monthvalues__value">{currentMonth.budget}$</span></p>
     </div>  
     </>
    );
